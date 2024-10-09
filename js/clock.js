6b30ff283451dfc27e789097bf669aefe1b74ecf
githat-cmd <haseebhumann@gmail.com>
2024-10-09 08:00:29 +0530

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2024-10-09 20:30", "Asia/Kolkata");
+  let targetDate = moment.tz("2024-10-14 20:30", "Asia/Kolkata");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
